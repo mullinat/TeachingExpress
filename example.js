1d8fe643d0a78d5bd11c32adac97ab0e07ab3ead
Your Name <you@example.com>
2018-04-17 18:55:57 -0400

    Added login page

--- example.js
+++ example.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 var html = "<p>Login</p><br><input type='text'></input>"
-app.get('/', function(req, res){
+app.get('/hello', function(req, res){
   res.send('I Like Pie');
 });
 app.get('/login', function(req, res){
     res.send(html);
   });
+app.use('/', express.static('src'));
 
 app.listen(3000);
